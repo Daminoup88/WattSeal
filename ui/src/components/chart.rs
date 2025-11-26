0cd54ce42f7d66e39badb72987bdaf21876abb96
Damien <damien.philippe@edu.esiee.fr>
2025-11-26 08:58:08 +0100

    Move the build chart utility inside the SensorChart impl

--- ui/src/components/chart.rs
+++ ui/src/components/chart.rs
@@ -112,6 +112,95 @@ impl<const N: usize> SensorChart<N> {
             .push(ChartWidget::new(self).height(Length::Fixed(chart_height)))
             .into()
     }
+
+    fn build_chart_2d<DB: DrawingBackend>(&self, mut chart: ChartBuilder<DB>) {
+        use plotters::prelude::*;
+
+        let newest_time = self
+            .data_series
+            .iter()
+            .flat_map(|series| series.data.front().map(|(time, _)| *time))
+            .max()
+            .unwrap_or_else(|| Utc::now());
+        let oldest_time = newest_time - chrono::Duration::seconds(PLOT_SECONDS as i64);
+
+        let mut chart = chart
+            .x_label_area_size(40)
+            .y_label_area_size(28)
+            .margin(20)
+            .build_cartesian_2d(oldest_time..newest_time, 0.0f32..100.0f32)
+            .expect("failed to build chart");
+
+        chart
+            .configure_mesh()
+            .bold_line_style(self.color.mix(0.1))
+            .light_line_style(self.color.mix(0.05))
+            .axis_style(ShapeStyle::from(self.color.mix(0.45)).stroke_width(1))
+            .y_labels(10)
+            .y_label_style(
+                ("sans-serif", 15)
+                    .into_font()
+                    .color(&self.color.mix(0.65))
+                    .transform(FontTransform::Rotate90),
+            )
+            .y_label_formatter(&|y: &f32| format!("{}%", y))
+            .y_desc("Usage (%)")
+            .x_labels(10)
+            .x_label_style(("sans-serif", 15).into_font().color(&self.color.mix(0.65)))
+            .x_labels(60)
+            .x_label_formatter(&|x: &DateTime<Utc>| {
+                let t = (newest_time.timestamp_millis() - x.timestamp_millis()) / 1000;
+                if t % 5 == 0 { format!("{}", t) } else { "".to_string() }
+            })
+            .x_desc("Time (s)")
+            .draw()
+            .expect("failed to draw chart mesh");
+
+        for series in &self.data_series {
+            let series_anno = match series.line_type {
+                LineType::Line => chart.draw_series(LineSeries::new(series.iter(), series.color)),
+                LineType::Area => chart.draw_series(
+                    AreaSeries::new(series.vec(), 0.0, series.color.mix(0.2))
+                        .border_style(ShapeStyle::from(series.color).stroke_width(2)),
+                ),
+                LineType::Dotted => chart.draw_series(DottedLineSeries::new(series.vec(), 5, 10, {
+                    let color = series.color;
+                    move |(x, y)| Circle::new((x, y), 3, color.filled())
+                })),
+                LineType::Points => chart.draw_series(PointSeries::of_element(
+                    series.iter(),
+                    5,
+                    &series.color,
+                    &|coord, size, style| EmptyElement::at(coord) + Circle::new((0, 0), size, style.filled()),
+                )),
+                LineType::Dashed => chart.draw_series(DashedLineSeries::new(
+                    series.vec(),
+                    5,
+                    10,
+                    ShapeStyle {
+                        color: series.color.to_rgba(),
+                        filled: false,
+                        stroke_width: 1,
+                    },
+                )),
+            };
+
+            series_anno
+                .expect("failed to draw chart data")
+                .label(&series.label)
+                .legend({
+                    let color = series.color;
+                    move |(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], color.stroke_width(2))
+                });
+        }
+
+        chart
+            .configure_series_labels()
+            .border_style(&BLACK)
+            .background_style(&WHITE.mix(0.8))
+            .draw()
+            .expect("failed to draw legend");
+    }
 }
 
 impl<const N: usize> Chart<Message> for SensorChart<N> {
@@ -123,96 +212,6 @@ impl<const N: usize> Chart<Message> for SensorChart<N> {
     }
 
     fn build_chart<DB: DrawingBackend>(&self, _state: &Self::State, chart: ChartBuilder<DB>) {
-        build_chart_2d(chart, &self.data_series, &self.color);
+        self.build_chart_2d(chart);
     }
-}
-
-fn build_chart_2d<DB: DrawingBackend>(mut chart: ChartBuilder<DB>, data_series: &[TimeSeries], color: &RGBColor) {
-    use plotters::prelude::*;
-    const PLOT_LINE_COLOR: RGBColor = RGBColor(0, 175, 255);
-
-    // Acquire time range
-    let newest_time = data_series
-        .iter()
-        .flat_map(|series| series.data.front().map(|(time, _)| *time))
-        .max()
-        .unwrap_or_else(|| Utc::now());
-    let oldest_time = newest_time - chrono::Duration::seconds(PLOT_SECONDS as i64);
-
-    let mut chart = chart
-        .x_label_area_size(40)
-        .y_label_area_size(28)
-        .margin(20)
-        .build_cartesian_2d(oldest_time..newest_time, 0.0f32..100.0f32)
-        .expect("failed to build chart");
-
-    chart
-        .configure_mesh()
-        .bold_line_style(color.mix(0.1))
-        .light_line_style(color.mix(0.05))
-        .axis_style(ShapeStyle::from(color.mix(0.45)).stroke_width(1))
-        .y_labels(10)
-        .y_label_style(
-            ("sans-serif", 15)
-                .into_font()
-                .color(&color.mix(0.65))
-                .transform(FontTransform::Rotate90),
-        )
-        .y_label_formatter(&|y: &f32| format!("{}%", y))
-        .y_desc("Usage (%)")
-        .x_labels(10)
-        .x_label_style(("sans-serif", 15).into_font().color(&color.mix(0.65)))
-        .x_labels(60)
-        .x_label_formatter(&|x: &DateTime<Utc>| {
-            let t = (newest_time.timestamp_millis() - x.timestamp_millis()) / 1000;
-            if t % 5 == 0 { format!("{}", t) } else { "".to_string() }
-        })
-        .x_desc("Time (s)")
-        .draw()
-        .expect("failed to draw chart mesh");
-
-    for series in data_series {
-        let series_anno = match series.line_type {
-            LineType::Line => chart.draw_series(LineSeries::new(series.iter(), series.color)),
-            LineType::Area => chart.draw_series(
-                AreaSeries::new(series.vec(), 0.0, series.color.mix(0.2))
-                    .border_style(ShapeStyle::from(series.color).stroke_width(2)),
-            ),
-            LineType::Dotted => chart.draw_series(DottedLineSeries::new(series.vec(), 5, 10, {
-                let color = series.color;
-                move |(x, y)| Circle::new((x, y), 3, color.filled())
-            })),
-            LineType::Points => chart.draw_series(PointSeries::of_element(
-                series.iter(),
-                5,
-                &series.color,
-                &|coord, size, style| EmptyElement::at(coord) + Circle::new((0, 0), size, style.filled()),
-            )),
-            LineType::Dashed => chart.draw_series(DashedLineSeries::new(
-                series.vec(),
-                5,
-                10,
-                ShapeStyle {
-                    color: series.color.to_rgba(),
-                    filled: false,
-                    stroke_width: 1,
-                },
-            )),
-        };
-
-        series_anno
-            .expect("failed to draw chart data")
-            .label(&series.label)
-            .legend({
-                let color = series.color;
-                move |(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], color.stroke_width(2))
-            });
-    }
-
-    chart
-        .configure_series_labels()
-        .border_style(&BLACK)
-        .background_style(&WHITE.mix(0.8))
-        .draw()
-        .expect("failed to draw legend");
 }
