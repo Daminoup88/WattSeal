21501ba68510899e2f5661d30c908b0285dac4f7
Gabin Dudillieu <gabin.dudillieu@liant.dev>
2026-04-30 11:03:24 +0200

    refactor(mqtt): Use SensorData function for topics

--- mqtt/src/topics.rs
+++ mqtt/src/topics.rs
@@ -2,15 +2,10 @@ use common::types::SensorData;
 
 pub fn sensor_data_to_topic(id: &str, sensor_data: &SensorData) -> String {
     let topic = "sensor_data";
-    match sensor_data {
-        SensorData::CPU(_) => format!("{}/{}/cpu", id, topic),
-        SensorData::GPU(_) => format!("{}/{}/gpu", id, topic),
-        SensorData::Ram(_) => format!("{}/{}/ram", id, topic),
-        SensorData::Disk(_) => format!("{}/{}/disk", id, topic),
-        SensorData::Network(_) => format!("{}/{}/network", id, topic),
-        SensorData::Total(_) => format!("{}/{}/total", id, topic),
-        SensorData::Process(_) => format!("{}/{}/process", id, topic),
-    }
+    let type_topic = sensor_data.sensor_type().to_lowercase();
+
+    println!("{type_topic}");
+    format!("{}/{}/{}", id, topic, type_topic)
 }
 
 pub fn hardware_info_topic(id: &str) -> String {
