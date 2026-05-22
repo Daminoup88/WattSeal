8f00d0bbf5b37af7cbb4e0e2102727f611224776
GabinDDL <gdudillieu@gmail.com>
2026-05-22 16:52:07 +0200

    feat(sensors/database): Adapt sensors database functions to new database data

--- collector/src/database/mod.rs
+++ collector/src/database/mod.rs
@@ -1,6 +1,9 @@
+pub mod process;
 mod tables;
 
 pub use common::{
     DATABASE_PATH, Database, DatabaseEntry, Event,
-    types::{CPUData, DiskData, GPUData, NetworkData, ProcessData, RamData, SensorData, TotalData},
+    database::types::{
+        CPUDataDB, DiskDataDB, GPUDataDB, NetworkDataDB, ProcessDataDB, RamDataDB, SensorDataDB, TotalDataDB,
+    },
 };
